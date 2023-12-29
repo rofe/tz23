a6f531680f47c7e8ba9fe0c54faad0d317d3cdd6
rofe <github@rofe.com>
2023-12-29 17:18:06 +0100

    chore: no toc entry for first heading

--- blocks/toc/toc.js
+++ blocks/toc/toc.js
@@ -1,16 +1,16 @@
 export default function decorate(block) {
   const ul = document.createElement('ul');
   document.querySelectorAll('h2').forEach((heading, i) => {
-    const li = document.createElement('li');
-    const jumpLink = document.createElement('a');
-    const title = heading.textContent;
-    jumpLink.textContent = title;
-    jumpLink.title = title;
-    jumpLink.href = `#${title.toLocaleLowerCase().replaceAll(' ', '-')}`;
-    li.append(jumpLink);
-    ul.append(li);
-
     if (i > 0) {
+      const li = document.createElement('li');
+      const jumpLink = document.createElement('a');
+      const title = heading.textContent;
+      jumpLink.textContent = title;
+      jumpLink.title = title;
+      jumpLink.href = `#${title.toLocaleLowerCase().replaceAll(' ', '-')}`;
+      li.append(jumpLink);
+      ul.append(li);
+
       const topLink = document.createElement('a');
       topLink.textContent = '^';
       topLink.className = 'toc-back-to-top';
