a44e25d5cbd8daa013ecdc99facf5297452bbaca
rofe <github@rofe.com>
2024-01-05 12:26:35 +0100

    fix: don't show TOC if 1 chapter

--- blocks/toc/toc.js
+++ blocks/toc/toc.js
@@ -1,24 +1,27 @@
 export default function decorate(block) {
   const ul = document.createElement('ul');
-  document.querySelectorAll('h2').forEach((heading, i) => {
-    if (i > 0) {
-      const li = document.createElement('li');
-      const jumpLink = document.createElement('a');
-      const title = heading.textContent;
-      jumpLink.textContent = title;
-      jumpLink.title = title;
-      jumpLink.href = `#${heading.id}`;
-      li.append(jumpLink);
-      ul.append(li);
+  const chapters = document.querySelectorAll('h2');
+  if (chapters.length > 1) {
+    chapters.forEach((heading, i) => {
+      if (i > 0) {
+        const li = document.createElement('li');
+        const jumpLink = document.createElement('a');
+        const title = heading.textContent;
+        jumpLink.textContent = title;
+        jumpLink.title = title;
+        jumpLink.href = `#${heading.id}`;
+        li.append(jumpLink);
+        ul.append(li);
 
-      const topLink = document.createElement('a');
-      topLink.textContent = '^';
-      topLink.className = 'toc-back-to-top';
-      topLink.title = 'Zurück nach oben';
-      topLink.href = '#';
-      heading.append(topLink);
-    }
-  });
-  block.textContent = 'Inhalt:';
-  block.append(ul);
+        const topLink = document.createElement('a');
+        topLink.textContent = '^';
+        topLink.className = 'toc-back-to-top';
+        topLink.title = 'Zurück nach oben';
+        topLink.href = '#';
+        heading.append(topLink);
+      }
+    });
+    block.textContent = 'Inhalt:';
+    block.append(ul);
+  }
 }
